ad57a6742df0a500e6d958b7cda98603d4d9528d
Ali <itsaliabdallah@gmail.com>
2023-07-03 14:52:49 +0100

    add styling to stable page & modal and move state so I can move the button for modal

--- src/components/stable/CreatePotWizard.tsx
+++ src/components/stable/CreatePotWizard.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { api } from "~/utils/api";
 import Modal from "react-modal";
+import { BsXCircleFill } from "react-icons/bs";
 
-export default function CreatePotWizard() {
+export default function CreatePotWizard({
+  modalIsOpen,
+  setModalIsOpen,
+}: {
+  modalIsOpen: boolean;
+  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const [name, setName] = useState<string>("");
   const [float, setFloat] = useState<number>(0);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   const ctx = api.useContext();
   const { data: userData, isLoading: userLoading } =
     api.users.getCurrentUserInfo.useQuery();
@@ -22,48 +28,61 @@ export default function CreatePotWizard() {
 
   return (
     <>
-      <button onClick={() => setModalIsOpen(true)}>Add New</button>
-
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        <form className="flex flex-col gap-4 p-4">
-          <label
-            className="w-28 self-center text-center text-lg"
-            htmlFor="nameInput"
-          >
-            Name:
-          </label>
-          <input
-            id="nameInput"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-72 self-center rounded-md border border-gray-300 p-2"
-          />
-
-          <label
-            className="w-28 self-center text-center text-lg "
-            htmlFor="floatInput"
-          >
-            Float:
-          </label>
-          <input
-            id="floatInput"
-            type="number"
-            value={float}
-            onChange={(e) => setFloat(parseFloat(e.target.value))}
-            className="w-72 self-center rounded-md border border-gray-300 p-2 text-right"
-          />
-
+      <Modal
+        style={{
+          overlay: { background: "#232931" },
+          content: { background: "#393e46" },
+        }}
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+      >
+        <div className="flex flex-col items-center">
           <button
-            type="submit"
-            disabled={isLoading}
-            onClick={handleSubmit}
-            className="w-28 self-center rounded-md bg-blue-500 p-2 text-white"
+            className="w-fit rounded-lg bg-theme-header p-2 text-2xl text-white"
+            onClick={() => setModalIsOpen(false)}
           >
-            Submit
+            <BsXCircleFill />
           </button>
-          {data && <p>Pot created!</p>}
-        </form>
+          <form className="flex flex-col gap-4 p-4">
+            <label
+              className="w-28 self-center text-center text-lg"
+              htmlFor="nameInput"
+            >
+              Name:
+            </label>
+            <input
+              id="nameInput"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-72 self-center rounded-lg p-2"
+            />
+
+            <label
+              className="w-28 self-center text-center text-lg "
+              htmlFor="floatInput"
+            >
+              Float:
+            </label>
+            <input
+              id="floatInput"
+              type="number"
+              value={float}
+              onChange={(e) => setFloat(parseFloat(e.target.value))}
+              className="w-72 self-center rounded-lg p-2 text-right"
+            />
+
+            <button
+              type="submit"
+              disabled={isLoading}
+              onClick={handleSubmit}
+              className="w-28 self-center rounded-md bg-blue-500 p-2 text-white"
+            >
+              Submit
+            </button>
+            {data && <p>Pot created!</p>}
+          </form>
+        </div>
       </Modal>
     </>
   );
--- src/pages/stable/index.tsx
+++ src/pages/stable/index.tsx
@@ -1,27 +1,51 @@
 import { SignedIn } from "@clerk/nextjs";
 import Link from "next/link";
+import { useState } from "react";
 import Loading from "~/components/Loading";
 import CreatePotWizard from "~/components/stable/CreatePotWizard";
 import { api } from "~/utils/api";
+import { AiOutlineFileAdd } from "react-icons/ai";
+
+export type CreatePotWizardType = {
+  modalIsOpen: boolean;
+  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 export default function Pots() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
     <SignedIn>
       <div className="mt-128 h-screen overflow-y-auto bg-theme-black text-white">
-        <PotsList />
-        <CreatePotWizard />
+        <PotsList setModalIsOpen={setModalIsOpen} />
+        <CreatePotWizard
+          modalIsOpen={modalIsOpen}
+          setModalIsOpen={setModalIsOpen}
+        />
       </div>
     </SignedIn>
   );
 }
 
-export function PotsList() {
+export function PotsList({
+  setModalIsOpen,
+}: {
+  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const { data, isLoading } = api.pots.list.useQuery();
   if (isLoading) return <Loading />;
   if (!data) return <p>No pots yet</p>;
   return (
     <div className="m-2 flex flex-col ">
-      <h2 className="text-center">Pots you have access to</h2>
+      <div className="flex justify-between p-2">
+        <h2 className="p-3 text-xl font-semibold">My Pots</h2>
+        <button
+          className="rounded-lg bg-theme-header px-2 text-2xl text-white"
+          onClick={() => setModalIsOpen(true)}
+        >
+          <AiOutlineFileAdd />
+        </button>
+      </div>
       {data.map((pot) => {
         return (
           <Link
