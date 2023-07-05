1aae997d16f2e843adff8837b20406b33c771b49
Ali <itsaliabdallah@gmail.com>
2023-07-05 14:48:00 +0100

    add styling to add friend modal

--- src/components/individual-horse/AddPlayerToPot.tsx
+++ src/components/individual-horse/AddPlayerToPot.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { api } from "~/utils/api";
 import Loading from "../Loading";
 import ConfirmButton from "../confirm-button/ConfirmButton";
+import { FiUserPlus } from "react-icons/fi";
 
 export default function AddPlayerToPot({
   pot_id,
@@ -17,14 +18,14 @@ export default function AddPlayerToPot({
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      className="fixed inset-0 flex items-center justify-center bg-theme-black bg-opacity-90 text-center"
       onClick={onClose}
     >
       <div
-        className="rounded-lg bg-white p-6"
+        className="rounded-lg bg-theme-black p-6 text-white"
         onClick={(e) => e.stopPropagation()}
       >
-        <label htmlFor="search-add-pot">Search for a friend</label>
+        <label htmlFor="search-add-pot">Add a friend</label>
         {/* <input
           id="search-add-pot"
           type="text"
@@ -35,8 +36,11 @@ export default function AddPlayerToPot({
         /> */}
         {data.map((user) => {
           return (
-            <div key={user.id}>
-              <p className="text-lg font-bold">{user.username}</p>
+            <div
+              className="m-2 flex justify-between rounded-lg bg-theme-grey p-2"
+              key={user.id}
+            >
+              <p className="p-2 text-lg font-bold">{user.username}</p>
               <AddToPotButton user_id={user.id} pot_id={pot_id} />
             </div>
           );
@@ -66,22 +70,22 @@ export function AddToPotButton({
     void ctx.friends.invalidate();
   }
   return (
-    <div className="flex flex-col items-center">
-      <ConfirmButton
-        className="rounded-md bg-blue-500 p-2"
-        onConfirm={handleClick}
-        disabled={isLoading}
-        buttonLabel="Add Player to Pot"
-        confirmMessage="Confirm?"
-      />
-      <label>
-        As Backer?:
+    <div className="flex items-center">
+      <div className="flex flex-col">
+        <label className="text-sm font-bold">As Backer?:</label>
         <input
           type="checkbox"
           checked={!!isBacker}
           onChange={handleCheckboxChange}
         />
-      </label>
+      </div>
+      <ConfirmButton
+        className="ml-2 rounded-md bg-blue-500 p-2 text-xl"
+        onConfirm={handleClick}
+        disabled={isLoading}
+        buttonLabel={<FiUserPlus />}
+        // confirmMessage="Confirm?"
+      />
       {!!data && <p className="text-green-500">Player Added</p>}
       {isError && <p className="text-red-500">Error</p>}
     </div>
