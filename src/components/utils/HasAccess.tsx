5c86da746a26e6de4e2b26eaba97f4253a0a5cd9
Mark Tyree <tyreemarka@gmail.com>
2023-08-20 14:35:42 +0100

    change from returning null to showing error when trying to access a pot that isn't found

--- src/components/utils/HasAccess.tsx
+++ src/components/utils/HasAccess.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from "react";
 import { api } from "~/utils/api";
 import Loading from "../Loading";
+import NotFound from "../errors/NotFound";
 
 export function HasAccess({
   pot_id,
@@ -8,6 +9,6 @@ export function HasAccess({
 }: { pot_id: number } & PropsWithChildren) {
   const { data, isLoading } = api.potAccess.hasAccess.useQuery({ pot_id });
   if (isLoading) return <Loading />;
-  if (!data) return null;
+  if (!data) return <NotFound page="page" />;
   return <>{children}</>;
 }
