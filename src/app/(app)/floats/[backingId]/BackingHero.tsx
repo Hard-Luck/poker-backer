63293916d217ca317b20a7f9c1f1fe59d19389d6
Mark Tyree <tyreemarka@gmail.com>
2025-01-25 17:13:11 +0000

    changed the delimiter to a semicolon to prevent confusion

--- src/app/(app)/floats/[backingId]/BackingHero.tsx
+++ src/app/(app)/floats/[backingId]/BackingHero.tsx
@@ -19,12 +19,12 @@ const DashboardCards: React.FC<DashboardCardsProps> = ({
   return (
     <section className="flex justify-between p-6 text-sm">
       <div className="flex flex-col ">
-        <div>Balance - £{currentFloat}</div>
-        <div>Sessions - Since Last Chop: {sessionsSinceLastChop}</div>
-        <div>Sessions - Total: {totalSessions}</div>
-        <div>Float - £{float}</div>
+        <div>Balance: £{currentFloat}</div>
+        <div>Sessions: Since Last Chop: {sessionsSinceLastChop}</div>
+        <div>Sessions: Total: {totalSessions}</div>
+        <div>Float: £{float}</div>
         <div>
-          Make up -
+          Make up:{" "}
           <span className={`${textColor}`}> £{Math.abs(profitOrLoss)}</span>
         </div>
       </div>
