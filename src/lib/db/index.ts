8ca9a23086bb324074242cdf6f8bd5ec0656a955
Hard-Luck <tyreemarka@gmail.com>
2026-04-23 19:59:49 +0100

    attempted fix from github

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,25 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
+
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var
   var db: PrismaClient | undefined;
 }
-const url = process.env.TURSO_DATABASE_URL as string;
-const authToken = process.env.TURSO_AUTH_TOKEN as string;
-
-let libsql;
-if (process.env.NODE_ENV === "production") {
-  libsql = createClient({
-    url,
-    authToken,
-  });
-} else {
-  libsql = createClient({ url: `file:${process.cwd()}/prisma/dev.db` });
-}
 
 if (!global.db) {
+  const libsql =
+    process.env.NODE_ENV === "production"
+      ? createClient({
+          url: (process.env.TURSO_DATABASE_URL as string).replace(
+            "libsql://",
+            "https://"
+          ),
+          authToken: process.env.TURSO_AUTH_TOKEN as string,
+        })
+      : createClient({ url: `file:${process.cwd()}/prisma/dev.db` });
+
   const adapter = new PrismaLibSQL(libsql);
   global.db = new PrismaClient({ adapter });
 }
